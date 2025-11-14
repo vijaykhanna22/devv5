e4d0d61ed95be6a987117d37f86838930de44f81
unknown <vijaykhanna@karunya.edu.in>
2025-11-15 00:10:33 +0530

    init lab

---
+++ tests/test_app.py
@@ -0,0 +1,9 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from app import add
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
